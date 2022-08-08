3fae17635a23c045d6f3b086b793a999e722851c
andres-geotec <andres.geotec@gmail.com>
2022-08-08 16:02:58 -0500

    attributos de la clase cluster

--- src/DisplacedPoints.js
+++ src/DisplacedPoints.js
@@ -2,7 +2,7 @@
  * @module DisplacedPoints
  */
 
-import VectorSource from "ol/source/Vector.js";
+import VectorSource from "ol/source/Vector";
 
 import methodsPlacement from "./methodsPlacement";
 
@@ -34,6 +34,69 @@ class DisplacedPoints extends VectorSource {
      * @protected
      */
     this.radioDisplacedPoints = options.radioDisplacedPoints || 6;
+
+    /**
+     * @type {number|undefined}
+     * @protected
+     */
+    this.resolution = undefined;
+
+    /**
+     * @type {number}
+     * @protected
+     */
+    this.distance = options.distance !== undefined ? options.distance : 20;
+
+    /**
+     * @type {number}
+     * @protected
+     */
+    this.minDistance = options.minDistance || 0;
+
+    /**
+     * @type {number}
+     * @protected
+     */
+    this.interpolationRatio = 0;
+
+    /**
+     * @type {Array<Feature>}
+     * @protected
+     */
+    this.features = [];
+
+    /**
+     * @param {Feature} feature Feature.
+     * @return {Point} Cluster calculation point.
+     * @protected
+     */
+    this.geometryFunction =
+      options.geometryFunction ||
+      function (feature) {
+        const geometry = /** @type {Point} */ (feature.getGeometry());
+        assert(geometry.getType() == "Point", 10); // The default `geometryFunction` can only handle `Point` geometries
+        return geometry;
+      };
+
+    /**
+     * @type {function(Point, Array<Feature>):Feature}
+     * @private
+     */
+    this.createCustomCluster_ = options.createCluster;
+
+    /**
+     * @type {VectorSource|null}
+     * @protected
+     */
+    this.source = null;
+
+    /**
+     * @private
+     */
+    this.boundRefresh_ = this.refresh.bind(this);
+
+    // this.updateDistance(this.distance, this.minDistance);
+    // this.setSource(options.source || null);
   }
 }
 
