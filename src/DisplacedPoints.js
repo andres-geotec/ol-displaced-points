ee50e648cd748a182ab548d3690a23bc1cc2e34d
andres-geotec <andres.geotec@gmail.com>
2022-08-08 15:38:22 -0500

    extendiendo clase de VectorSource

--- src/DisplacedPoints.js
+++ src/DisplacedPoints.js
@@ -1,30 +1,39 @@
-import { Cluster } from "ol/source";
+/**
+ * @module DisplacedPoints
+ */
 
-class DisplacedPoints extends Cluster {
+import VectorSource from "ol/source/Vector.js";
+
+import methodsPlacement from "./methodsPlacement";
+
+class DisplacedPoints extends VectorSource {
   /**
    * @param {Options} options DisplacedPoints options.
    */
   constructor(options) {
     console.log("instancioansdo DisplacedPoints");
-    super(options);
+    super({
+      attributions: options.attributions,
+      wrapX: options.wrapX,
+    });
+
+    /**
+     * @type {string}
+     * @protected
+     */
+    this.methodPlacement = methodsPlacement[options.methodPlacement || "ring"];
 
     /**
      * @type {number}
      * @protected
      */
-     this.methodPlacement = methodsPlacement_[options.methodPlacement || "ring"];
+    this.radioCenterPoint = options.radioCenterPoint || 6;
 
-     /**
-      * @type {number}
-      * @protected
-      */
-     this.radioCenterPoint = options.radioCenterPoint || 6;
- 
-     /**
-      * @type {number}
-      * @protected
-      */
-     this.radioDisplacedPoints = options.radioDisplacedPoints || 6;
+    /**
+     * @type {number}
+     * @protected
+     */
+    this.radioDisplacedPoints = options.radioDisplacedPoints || 6;
   }
 }
 
