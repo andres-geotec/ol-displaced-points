fd1c7d0ba7102baaf618db16aca72338680f791f
andres-geotec <andres.geotec@gmail.com>
2022-08-08 16:50:12 -0500

    funciones de cluster

--- src/DisplacedPoints.js
+++ src/DisplacedPoints.js
@@ -4,6 +4,17 @@
 
 import VectorSource from "ol/source/Vector";
 import EventType from "ol/events/EventType";
+import {
+  createEmpty,
+  createOrUpdateFromCoordinate,
+  buffer,
+  getCenter,
+} from "ol/extent";
+import { getUid } from "ol/util";
+import { assert } from "ol/asserts";
+import { add as addCoordinate, scale as scaleCoordinate } from "ol/coordinate";
+import Point from "ol/geom/Point";
+import Feature from "ol/Feature";
 
 import methodsPlacement from "./methodsPlacement";
 
@@ -176,8 +187,121 @@ class DisplacedPoints extends VectorSource {
     this.minDistance = minDistance;
     this.interpolationRatio = ratio;
     if (changed) {
-      // this.refresh();
+      this.refresh();
     }
+  }
+
+  /**
+   * @param {import("ol/extent").Extent} extent Extent.
+   * @param {number} resolution Resolution.
+   * @param {import("ol/proj/Projection").default} projection Projection.
+   */
+  loadFeatures(extent, resolution, projection) {
+    this.source.loadFeatures(extent, resolution, projection);
+    if (resolution !== this.resolution) {
+      this.resolution = resolution;
+      this.refresh();
+    }
+  }
+
+  /**
+   * Handle the source changing.
+   */
+  refresh() {
+    this.clear();
+    this.cluster();
+    this.addFeatures(this.features);
+  }
+
+  /**
+   * @protected
+   */
+  cluster() {
+    if (this.resolution === undefined || !this.source) {
+      return;
+    }
+    const extent = createEmpty();
+    const mapDistance = this.distance * this.resolution;
+    const features = this.source.getFeatures();
+
+    /** @type {Object<string, true>} */
+    const clustered = {};
+
+    for (let i = 0, ii = features.length; i < ii; i++) {
+      const feature = features[i];
+      if (!(getUid(feature) in clustered)) {
+        const geometry = this.geometryFunction(feature);
+        if (geometry) {
+          const coordinates = geometry.getCoordinates();
+          createOrUpdateFromCoordinate(coordinates, extent);
+          buffer(extent, mapDistance, extent);
+
+          const neighbors = this.source
+            .getFeaturesInExtent(extent)
+            .filter(function (neighbor) {
+              const uid = getUid(neighbor);
+              if (uid in clustered) {
+                return false;
+              }
+              clustered[uid] = true;
+              return true;
+            });
+          this.features.push(this.createCluster(neighbors, extent));
+        }
+      }
+    }
+  }
+
+  /**
+   * @param {Array<Feature>} features Features
+   * @param {import("ol/extent").Extent} extent The searched extent for these features.
+   * @return {Feature} The cluster feature.
+   * @protected
+   */
+  createCluster(features, extent) {
+    const centroid = [0, 0];
+    for (let i = features.length - 1; i >= 0; --i) {
+      const geometry = this.geometryFunction(features[i]);
+      if (geometry) {
+        addCoordinate(centroid, geometry.getCoordinates());
+      } else {
+        features.splice(i, 1);
+      }
+    }
+    scaleCoordinate(centroid, 1 / features.length);
+    const searchCenter = getCenter(extent);
+    const ratio = this.interpolationRatio;
+
+    return this.createFeatureCluster(
+      new Point([
+        centroid[0] * (1 - ratio) + searchCenter[0] * ratio,
+        centroid[1] * (1 - ratio) + searchCenter[1] * ratio,
+      ]),
+      features
+    );
+  }
+
+  /**
+   * @protected
+   */
+  createFeatureCluster(geometry, features) {
+    if (this.createCustomCluster_) {
+      return this.createCustomCluster_(geometry, features);
+    }
+    return new Feature({
+      geometry,
+      features,
+    });
+  }
+
+  /**
+   * Remove all features from the source.
+   * @param {boolean} [opt_fast] Skip dispatching of {@link module:ol/source/VectorEventType~VectorEventType#removefeature} events.
+   * @api
+   */
+  clear(opt_fast) {
+    this.features.length = 0;
+    super.clear(opt_fast);
   }
 }
 
