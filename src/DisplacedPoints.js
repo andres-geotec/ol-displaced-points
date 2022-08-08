68e96b7921a4ce1939a0199cb8d8898cb65de58c
andres-geotec <andres.geotec@gmail.com>
2022-08-08 15:06:10 -0500

    parámetros default

--- src/DisplacedPoints.js
+++ src/DisplacedPoints.js
@@ -1,9 +1,30 @@
 import { Cluster } from "ol/source";
 
 class DisplacedPoints extends Cluster {
+  /**
+   * @param {Options} options DisplacedPoints options.
+   */
   constructor(options) {
-    super(options);
     console.log("instancioansdo DisplacedPoints");
+    super(options);
+
+    /**
+     * @type {number}
+     * @protected
+     */
+     this.methodPlacement = methodsPlacement_[options.methodPlacement || "ring"];
+
+     /**
+      * @type {number}
+      * @protected
+      */
+     this.radioCenterPoint = options.radioCenterPoint || 6;
+ 
+     /**
+      * @type {number}
+      * @protected
+      */
+     this.radioDisplacedPoints = options.radioDisplacedPoints || 6;
   }
 }
 
