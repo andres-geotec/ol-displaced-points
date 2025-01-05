b0945a8e4cbbf8170137c34c91ec60b6be2d3600
andres-geotec <andres.geotec@gmail.com>
2025-01-05 00:17:46 -0600

    🔧 feat: configuración para pages

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "ol-displaced-points",
-  description: "Displaced Points methodology works to visualize all features of a point layer, even if they have the same location. The map takes the points falling in a given Distance tolerance from each other (cluster) and places them around their barycenter."
+  description: "Displaced Points methodology works to visualize all features of a point layer, even if they have the same location. The map takes the points falling in a given Distance tolerance from each other (cluster) and places them around their barycenter.",
+  base: '/ol-displaced-points/',
 })
